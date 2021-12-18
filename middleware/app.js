61677cbe08cef47bc336449f563c7cae8d25359b
Yashas Kamath <81322656+YashasKamath@users.noreply.github.com>
2021-12-19 01:01:40 +0530

    Add files via upload

---
+++ middleware/app.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const url='mongodb://localhost/Decrypt'
+
+const app=express()
+
+mongoose.connect(url,{useNewUrlParser:true})
+const con=mongoose.connection
+con.on('open',()=>{
+    console.log('Connected...')
+})
+
+app.use(express.json())
+
+//const questionRouter=require('./QRoutes/questions')
+//app.use('/questions',questionRouter)
+
+const userRouter=require('./QRoutes/users')
+app.use('/users',userRouter)
+
+app.listen(9000, ()=>{
+    console.log("Server started")
+})
