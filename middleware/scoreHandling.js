0414b82b6fb74282584c2b9d0d16c0190ff737fa
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2021-12-22 16:09:59 +0530

    scoring logic

---
+++ middleware/scoreHandling.js
@@ -0,0 +1,27 @@
+function incrementScore(currentScore, isCorrect, attemptNumber) {
+  if (isCorrect) {
+    currentScore =
+      attemptNumber == 1
+        ? currentScore + 100
+        : attemptNumber == 2
+        ? currentScore + 90
+        : attemptNumber == 2
+        ? currentScore + 80
+        : currentScore + 50;
+  }
+  return currentScore;
+}
+
+function addStreakBonus(responses) {
+  var bonus = 0;
+  for (let i = 2; i < responses.length; i += 3) {
+    if (responses[i] && responses[i - 1] && responses[i - 2]) bonus += 20;
+  }
+  return bonus;
+}
+
+console.log(incrementScore(20, true, 1));
+console.log(addStreakBonus(50, [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1]));
+
+// to be done
+function scoreHandling() {}
