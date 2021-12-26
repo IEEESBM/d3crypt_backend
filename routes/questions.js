3ccded459fe43eefb5a8f5ac387e9c64c424d0cc
Yashas Kamath <81322656+YashasKamath@users.noreply.github.com>
2021-12-26 18:18:24 +0530

    Update questions.js

--- routes/questions.js
+++ routes/questions.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const router=express.Router()
-const Question=require('../Questions_Schema/question_schema.js')
+const Question=require('../models/question_schema.js')
 
 router.get('/',async(req,res)=>{
     try{
