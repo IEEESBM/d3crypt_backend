574e9b808fb4a1a730014dac3407e8433362935d
divyashree <divyamanipal01@gmail.com>
2022-02-13 15:11:06 +0530

    Changes

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -40,81 +40,81 @@ router.get("/", checkJWT, checkIsVerified, async (req, res) => {
   }
 });
 
-router.get("/show_all", async (req, res) => {
-  try {
-    const questions = await Question.find();
-    res.json(questions);
-  } catch (err) {
-    res.send("Error " + err);
-  }
-});
+// router.get("/show_all", async (req, res) => {
+//   try {
+//     const questions = await Question.find();
+//     res.json(questions);
+//   } catch (err) {
+//     res.send("Error " + err);
+//   }
+// });
 
-router.post("/add", async (req, res) => {
-  try {
-    const newq = await Question.create(
-      {
-        title: req.body.title,
-        difficulty: req.body.difficulty,
-        answer: req.body.answer,
-        index: req.body.index,
-        image_1: req.body.image_1,
-        image_2: req.body.image_2,
-        image_3: req.body.image_3,
-        image_4: req.body.image_4,
-        hint_1: req.body.hint_1,
-        hint_2: req.body.hint_2,
-      }
-      // ,
-      // function (err) {
-      //   console.log(err);
-      //   res.status(400).json(err);
-      // }
-    );
-    // console.log(newq);
-    res.send(newq);
-  }
-  catch (error) {
-    res.status(500).json(error);
-  }
-});
+// router.post("/add", async (req, res) => {
+//   try {
+//     const newq = await Question.create(
+//       {
+//         title: req.body.title,
+//         difficulty: req.body.difficulty,
+//         answer: req.body.answer,
+//         index: req.body.index,
+//         image_1: req.body.image_1,
+//         image_2: req.body.image_2,
+//         image_3: req.body.image_3,
+//         image_4: req.body.image_4,
+//         hint_1: req.body.hint_1,
+//         hint_2: req.body.hint_2,
+//       }
+//       // ,
+//       // function (err) {
+//       //   console.log(err);
+//       //   res.status(400).json(err);
+//       // }
+//     );
+//     // console.log(newq);
+//     res.send(newq);
+//   }
+//   catch (error) {
+//     res.status(500).json(error);
+//   }
+// });
 
-router.get("/:id", async (req, res) => {
-  try {
-    const question = await Question.findById(req.params.id);
-    res.json(question);
-  } catch (err) {
-    res.send("Error " + err);
-  }
-});
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const question = await Question.findById(req.params.id);
+//     res.json(question);
+//   } catch (err) {
+//     res.send("Error " + err);
+//   }
+// });
 
-router.patch("/:id", async (req, res) => {
-  try {
-    const q = await Question.findById(req.params.id);
-    const data = req.body;
-    q.image_1 = data.image_1;
-    q.image_2 = data.image_2;
-    q.image_3 = data.image_3;
-    q.image_4 = data.image_4;
-    q.question = data.question;
-    q.difficulty = data.difficulty;
-    q.points = data.points;
-    q.answer = data.answer;
-    q.hint = data.hint;
-    const a1 = await q.save();
-    res.json(a1);
-  } catch (err) {
-    res.send("Error " + err);
-  }
-});
+// router.patch("/:id", async (req, res) => {
+//   try {
+//     const q = await Question.findById(req.params.id);
+//     const data = req.body;
+//     q.image_1 = data.image_1;
+//     q.image_2 = data.image_2;
+//     q.image_3 = data.image_3;
+//     q.image_4 = data.image_4;
+//     q.question = data.question;
+//     q.difficulty = data.difficulty;
+//     q.points = data.points;
+//     q.answer = data.answer;
+//     q.hint = data.hint;
+//     const a1 = await q.save();
+//     res.json(a1);
+//   } catch (err) {
+//     res.send("Error " + err);
+//   }
+// });
 
-router.delete("/:id", async (req, res) => {
-  try {
-    const question = await Question.findById(req.params.id);
-    const a1 = await question.remove();
-    res.send("Removed");
-  } catch (err) {
-    res.send("Error " + err);
-  }
-});
+// router.delete("/:id", async (req, res) => {
+//   try {
+//     const question = await Question.findById(req.params.id);
+//     const a1 = await question.remove();
+//     res.send("Removed");
+//   } catch (err) {
+//     res.send("Error " + err);
+//   }
+// });
 
 module.exports = router;
